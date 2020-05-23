4e45b05fddfcc9eca68c4a642d8e500bd6019b51
Max Zhou <zhouyifan0904@gmail.com>
2020-05-23 13:55:12 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {
   Route,
   Link,
