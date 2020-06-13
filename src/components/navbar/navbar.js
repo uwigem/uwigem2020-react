a6d687c2fb425e7767b58fdec30db81f8fdab90f
Jennifer Tao <1365673620@qq.com>
2020-06-12 22:35:00 -0700

    Add projects page link

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,12 +3,13 @@ import './navbar.css';
 
 import { Link } from 'react-router-dom';
 
-const tabs = ['Home', 'About', 'Get Involved'];
+const tabs = ['Home', 'About', 'Projects', 'Get Involved'];
 
 const NavBar = props => {
 	return (
 		<div className='nav-bar-container'>
 			<Link className='nav-tab' to='/'>Home</Link>
+			<Link className='nav-tab' to='/projects'>Projects</Link>
 			<Link className='nav-tab' to='/about'>About</Link>
 			<Link className='nav-tab' to='/involvement'>Get Involved</Link>
 		</div>
