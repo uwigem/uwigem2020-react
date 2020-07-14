f80c78c52ecb13709ce29632d35d439cc63ef840
Victor Shan <victorshanvs@gmail.com>
2020-07-13 23:25:48 -0700

    added competition component to home for preview

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './home.css';
 import HomepageMainSection from "./MainSection/HomepageMainSection";
 import Cards from "./card/card";
+import Competition from './competition/competition';
 
 const HomePage = props => {
   return (
     <>
       <HomepageMainSection/>
       <Cards/>
+      <Competition 
+        imageSrc={"/logo512.png"} 
+        imageAlt={"React Logo"}
+        title={"Competition"}
+        subtitle={"Getting things done"}
+        bodyText={"This is the body text. Maybe it has a newline here.\n Otherwise, this will appear on the same line"}
+      />
     </>
   )
 }
