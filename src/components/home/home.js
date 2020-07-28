a1c5f98f9919bb0bb53c6a8bbc8c768e36e7fdf3
Max Zhou <zhouyifan0904@gmail.com>
2020-07-28 08:27:41 -0700

    updated carousel

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './home.css';
 import HomepageMainSection from "./MainSection/HomepageMainSection";
 import Cards from "./card/card";
+import HomeCarousel from './Carousel/HomeCarousel';
 
 const HomePage = props => {
   return (
     <>
+      <HomeCarousel />
       <HomepageMainSection/>
       <Cards/>
     </>
