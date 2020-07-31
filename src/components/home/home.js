d35c59ed008a1d46e3ebd61684333788d70183b5
Max Zhou <zhouyifan0904@gmail.com>
2020-07-31 14:42:53 -0700

    added stuff

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,11 +3,13 @@ import './home.css';
 import HomepageMainSection from "./MainSection/HomepageMainSection";
 import Cards from "./card/card";
 import HomeCarousel from './Carousel/HomeCarousel';
+import Insights from './Insights/Insights';
 
 const HomePage = props => {
   return (
     <>
       <HomeCarousel />
+      <Insights />
       <HomepageMainSection/>
       <Cards/>
     </>
