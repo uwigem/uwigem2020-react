46b59ab243b35628d4f91217100f7b051dc1c2d9
Max Zhou <zhouyifan0904@gmail.com>
2020-08-01 15:00:23 -0700

    added highlights layout

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,12 +4,14 @@ import HomepageMainSection from "./MainSection/HomepageMainSection";
 import Cards from "./card/card";
 import HomeCarousel from './Carousel/HomeCarousel';
 import Insights from './Insights/Insights';
+import Highlights from './Highlights/Highlights';
 
 const HomePage = props => {
   return (
     <>
       <HomeCarousel />
       <Insights />
+      <Highlights />
       <HomepageMainSection/>
       <Cards/>
     </>
