67d63482e180c7b99935412825f45d2ec3d71bd4
Victor Shan <victorshanvs@gmail.com>
2020-08-08 15:32:18 -0700

    Join us part complete with animations but not data

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './home.css';
 import HomepageMainSection from "./MainSection/HomepageMainSection";
 import Cards from "./card/card";
+import JoinUs from '../join-us/JoinUs';
 
 const HomePage = props => {
   return (
     <>
       <HomepageMainSection/>
       <Cards/>
+      <JoinUs />
     </>
   )
 }
