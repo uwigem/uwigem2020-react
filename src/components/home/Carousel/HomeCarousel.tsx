55744ee7a938e5c65707022ed038fcdd96ae1566
Max Zhou <zhouyifan0904@gmail.com>
2020-08-15 14:19:42 -0700

    added sponsor page

--- src/components/home/Carousel/HomeCarousel.tsx
+++ src/components/home/Carousel/HomeCarousel.tsx
@@ -14,64 +14,31 @@ const HomeCarousel = () => {
           <Carousel.Item>
             <img
               className="d-block w-100"
-              src="https://picsum.photos/id/1/2000/1000"
+              src={process.env.PUBLIC_URL + '/ocean.png'}
               alt="First slide"
             />
-            <Carousel.Caption>
-              <div className={s.captionContainer}>
-              <div className={s.caption}>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </div>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://picsum.photos/id/2/2000/1000"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <div className={s.captionContainer}>
-              <div className={s.caption}>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://picsum.photos/id/3/2000/1000"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <div className={s.captionContainer}>
-              <div className={s.caption}>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              </div>
-              </div>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        </div>
 
-        <div className={s.textContainer}>
-          <div className={s.title}>
-            <h1>Welcome to UW IGEM!</h1>
+        <div className={s.captionContainer}>
+          <div className={s.captionLeft}>
+            <div className={s.title}>
+              <h1>Welcome to iGEM</h1>
+            </div>
           </div>
-          <div className={s.intro}>
-            <p>Lorem ipsum</p>
+
+          <div className={s.captionRight}>
+            <div className={s.text}>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit cursus eleifend tincidunt tempor, mauris lobortis viverra id. Scelerisque et sit elementum in sed tincidunt at.
+              </p>
+            </div>
+            <div className={s.wikiButton}>
+              <button>
+                Current Wiki
+              </button>
+            </div>
           </div>
-          <div className={s.more}>
-            <button>Current Wiki</button>
           </div>
         </div>
       </div>
