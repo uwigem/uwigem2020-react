bf6425c56e8bcf300a1e1b56b9bb28a70e8ba7f4
jadens3 <49039591+jadens3@users.noreply.github.com>
2020-08-24 11:15:29 -0700

    Added logos

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import './navbar.css';
 
 import {Link} from 'react-router-dom';
-import {useLocation} from 'react-router-dom';
 
 const tabs = ['Homepage', 'Archive', 'Get Involved', 'Team', 'About'];
 const paths = ['/', '/archive', '/involvement', '/team', '/about'];
+const leftLogo = "https://2019.igem.org/wiki/images/3/32/T--Washington--HLogo2.png";
+const rightLogo = "https://2019.igem.org/wiki/images/3/32/T--Washington--HLogo2.png";
 
 const NavBar = () => {
 	const [tab, setTab] = useState(paths.indexOf(window.location.hash.substring(1)));
@@ -17,17 +18,17 @@ const NavBar = () => {
 			className='nav-tab'
 			to={paths[i]}
 			onClick={() => setTab(i)}
-			style={{borderBottom: tab === i ? '1vh solid #4B2E83' : 'none'}}
+			style={{borderBottom: tab === i ? '0.75vw solid #4B2E83' : 'none'}}
 		>
 			{tabs[i]}
 		</Link>)
 	}
 
 	return (
-		<div className='nav-bar-container'>
-			<img className='logo-left' src="logo192.png"/>
+		<div id='nav-bar-container'>
+			<img id='logo-left' className='logo' src={leftLogo} alt='Logo'/>
 			{links}
-			<img className='logo-right' src="logo192.png"/>
+			<img id='logo-right' className='logo' src={rightLogo} alt='Logo'/>
 		</div>
 	)
 }
