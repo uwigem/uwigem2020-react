357ec28ebfbdd70bdfdb06d11c1c64a35a6ef7c6
Jennifer Tao <1365673620@qq.com>
2020-09-04 22:38:15 -0700

    update old version timeline

--- src/components/team-history/teamHistory.js
+++ src/components/team-history/teamHistory.js
@@ -7,72 +7,72 @@ const TeamHistory = props =>{
 	const currentIndex = 2;
 	const dataSize = data.length;
 	const [index, setIndex] = useState(currentIndex);
-	const [left, setDir] = useState(false);
-	const [fade, setFade] = useState(false);
+	const [expanded, toggleMotto] = useState(false);
 	
 
 	return <>
-	    <div className="timeline-content">
-			<Fade left={!left} right={left} spy={fade} duration={300}>
-				<p className="timeline-header">1111{data[index].title}</p>
-				<p><em>{data[index].award}</em></p>
-				<p>{data[index].content}</p>
-			</Fade>
-			
+		<div className="timeline-content"
+			style={{visibility: expanded? "visible" : "hidden"}}>
+			<div className="timeline-background"></div>
+			<p className="timeline-header">{data[index].title}</p>
+			<p><em>{data[index].award}</em></p>
+			<p>{data[index].content}</p>
+		
+			<button className="exit-motto-button"
+				onClick={() => {toggleMotto(false)}}
+				>&lt; Back</button>
 		</div>
-		<div className="timeline">
+		<div className="timeline"
+			style={{visibility: !expanded? "visible" : "hidden"}}>
 			<div className="events-wrapper">
 				<div className="events">			
 					<ol> 
 						<li className={(index < 1)? "prev disable" : "prev"}
 							onClick={() => {
 								setIndex(index-1);
-								setDir(false);
-								setFade(!fade);
 							}}
 							><a></a></li>
 						<li><a className={(index<= 1)? "disable" : ""}
 							onClick={() => {
 								setIndex(index-2);
-								setDir(false);
-								setFade(!fade);
+								toggleMotto(true);
 							}}>
 							{(index <= 1)? "" : data[index-2].year}</a></li>
 						<li><a className={(index <= 0)? "disable" : ""}
 							onClick={() => {
 								setIndex(index-1);
-								setDir(false);
-								setFade(!fade);
+								toggleMotto(true);
 							}}>
 							{(index <= 0)? "" : data[index-1].year}</a></li>
 						<li className="active"><a
-							onClick={() => setIndex(index)}>{data[index].year}</a></li>
+							onClick={() => {
+								setIndex(index);
+								toggleMotto(true);
+							}}>
+							{data[index].year}</a></li>
 						<li className={(index >= dataSize-1)? "disable" : ""}><a
 							onClick={() => {
 								setIndex(index+1);
-								setDir(true);
-								setFade(!fade);
+								toggleMotto(true);
 							}}>
 							{(index >= dataSize-1)? "" : data[index+1].year}</a></li>
 						<li className={(index >= dataSize-2)? "disable" : ""}><a
 							onClick={() => {
 								setIndex(index+2);
-								setDir(true);
-								setFade(!fade);
+								toggleMotto(true);
 							}}>
 							{(index >= dataSize-2)? "" : data[index+2].year}</a></li>
 
 						<li className={(index > dataSize-2)? "next disable" : "next"}
 							onClick={() => {
 								setIndex(index+1);
-								setDir(true);
-								setFade(!fade);
 							}}><a></a></li>
 					</ol>
 				</div>
 			</div>
-			
 		</div>
+	
+	    
 	</>;
 }
 export default TeamHistory;
