3dac0d5748a30d8f74ec9f36c87510e6007bbcfc
jadens3 <49039591+jadens3@users.noreply.github.com>
2020-09-05 17:05:12 -0700

    implemented design

--- src/components/team/membersPage/teamMember.js
+++ src/components/team/membersPage/teamMember.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import Fade from 'react-reveal/Fade';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { Scrollbars } from 'react-custom-scrollbars';
@@ -16,25 +15,94 @@ const TeamMember = props => {
     return <>
         <div className="person" onClick={() => toggleExpand(!expanded)}>
             <section className="person-image">
-                <img src="http://placekitten.com/380/380"></img>
+                <img src={props.person.picture}></img>
             </section>
             <section className="person-name">{props.person.name}</section>
             {/* first element of person.teams is always 'all' */}
-
             <Modal
+                className="member-card"
                 show={expanded}
                 onHide={() => toggleExpand(!expanded)}
                 size="lg"
                 centered
             >
-                <Modal.Body>
-                    <img src="http://placekitten.com/380/380" style={{float: 'left',}}></img>
-                    <p>{props.person.name}</p>
-                    <p>Major: {props.person.major}</p>
-                    <p>Focus: {props.person.teams.toString()}</p>
+                <Modal.Body className="card-body">
+                    <img className="card-image" src={props.person.picture}></img>
+                    <div className="card-content">
+                        {
+                            props.person.pronouns?
+                            <>
+                                <p className='card-pronouns'>
+                                    <b className='card-name'>
+                                        {props.person.name}
+                                    </b>
+                                    {" (" + props.person.pronouns + ")"}
+                                </p>
+                            </>
+
+                            :
+
+                            <>
+                                <p className='card-name'>{props.person.name}</p>
+                            </>
+                        }
+                        <p className="card-text">
+                            Major:
+                            <b className='card-info'>
+                                {" " + props.person.major}
+                            </b>
+                        </p>
+                        <p className="card-text">Focus:
+                            <b className='card-info'>
+                                {props.person.teams.slice(1).map(s => " " + s).toString()}
+                            </b>
+                        </p>
+                        {
+                            props.person.bio?
+                                <>
+                                    <p className="card-bio">
+                                        {"\"" + props.person.bio + "\""}
+                                    </p>
+                                </>
+                            :
+                                <>
+                                </>
+                        }
+                    </div>
+                    <div className='card-links'>
+                        {
+                            props.person.linkedin?
+
+                            <>
+                                <a href={props.person.linkedin}>
+                                    <FontAwesomeIcon icon={faLinkedin} size={"3x"} color={"black"} className='card-logo'/>
+                                </a>
+                            </>
+
+                            :
+
+                            <>
+                                <FontAwesomeIcon icon={faLinkedin} size={"3x"} color={"grey"} className='card-logo'/>
+                            </>
+                        }
+                        {
+                            props.person.github?
+
+                            <>
+                                <a href={props.person.github}>
+                                    <FontAwesomeIcon icon={faGithub} size={"3x"} color={"black"} className='card-logo'/>
+                                </a>
+                            </>
+
+                            :
+
+                            <>
+                                <FontAwesomeIcon icon={faGithub} size={"3x"} color={"grey"} className='card-logo'/>
+                            </>
+                        }
+                    </div>
                 </Modal.Body>
             </Modal>
-                
         </div>
     </>;
 }
