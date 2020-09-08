58666eaa5df7e889b6fa8f34dbcbfd75438fe834
Victor Shan <victorshanvs@gmail.com>
2020-09-07 23:00:16 -0700

    Sidebar detects viewable element ratios

--- src/components/team/TeamPageSideBar.js
+++ src/components/team/TeamPageSideBar.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React,  { useEffect, useState } from 'react'
 import './TeamPageSideBar.css'
 
 /*
  * section list in the form [{ name: 'name', ref: React.Ref }]
  */
 export default function TeamPageSideBar({ sectionList }) {
+  
+  const observer = new IntersectionObserver((entries) => handleObserve(entries, sectionList), {
+    root: null, // default (null) to viewport
+    threshold: new Array(101).fill(0).map((v, i) => i * 0.01) // [0.0,0.1,0.2,0.3, ..., 1.0]
+  })
+  useEffect(() => {
+    sectionList.forEach(section => {
+      observer.observe(section.ref.current)
+    })
+    return () => {
+      observer.disconnect()
+      console.log("observer disconnected")
+    }
+  }, [sectionList, observer])
+
   return (
     <>
     {sectionList && sectionList.length > 0 &&
       <nav className={'team-page-side-bar'}>
         <ul>
-          {sectionList.map(headingToLink)}
+          {sectionList.map(sectionToLink)}
         </ul>
       </nav>}
     </> 
@@ -18,7 +33,7 @@ export default function TeamPageSideBar({ sectionList }) {
 }
 
 
-const headingToLink = (section) => {
+const sectionToLink = (section) => {
   return (
     <li key={section.name} onClick={() => scrollToRef(section.ref)}>
       {section.name}
@@ -27,3 +42,18 @@ const headingToLink = (section) => {
 }
 
 const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop)
+
+
+const handleObserve = (entries, sectionList) => {
+  console.log("Entries", entries.length)
+  for(let entry of entries) {
+    const entryName = sectionList.reduce((name, section) => {
+      if (name === "Unknown" && section.ref.current === entry.target) {
+        name = section.name
+      }
+      return name
+    }, "Unknown")
+    console.log("Entry " + entryName + ":", entry.intersectionRatio)
+  }
+
+}
