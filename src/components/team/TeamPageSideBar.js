5bbf588dadd288081833c76fbb5c073fda762d84
Victor Shan <victorshanvs@gmail.com>
2020-09-08 20:41:48 -0700

    Bolding sidebar works uses functional update

--- src/components/team/TeamPageSideBar.js
+++ src/components/team/TeamPageSideBar.js
@@ -6,28 +6,25 @@ import './TeamPageSideBar.css'
  * all sections must have an id tag same with the id field
  */
 export default function TeamPageSideBar({ sectionList }) {
+  const [currSection, setCurrSection] = useState('')
   const [sectionRatios, setSectionRatios] = useState({})
-
-  const handleObserve = (entries) => {
-    console.log(entries.map(s => s.target.id), sectionList.map(s => s.id));
-    console.log("Entries", entries.length)
-    for(let entry of entries) {
-      let entryName = 'Unknown'
-      for (let section of sectionList) {
-        if (section.id === entry.target.id) {
-          entryName = section.name
-          break
-        }
-      }
-      console.log("Entry " + entryName + ":", entry.intersectionRatio)
-    }
   
-  }
   
   useEffect(() => {
 
+    const handleObserve = (entries) => {
+      const newEntries = {}
+      for(let entry of entries) {
+        newEntries[entry.target.id] = entry.intersectionRatio
+      }
+      setSectionRatios(oldRatios => ({
+        ...oldRatios,
+        ...newEntries
+      }))
+    }
+
     // Setup observer
-    const observer = new IntersectionObserver((entries) => handleObserve(entries), {
+    const observer = new IntersectionObserver(entries => handleObserve(entries), {
       root: null, // default (null) to viewport
       threshold: new Array(101).fill(0).map((v, i) => i * 0.01)
     })
@@ -38,27 +35,36 @@ export default function TeamPageSideBar({ sectionList }) {
     
     return () => {
       observer.disconnect()
-      console.log("observer disconnected")
     }
   }, [sectionList])
+
+  useEffect(() => {
+    let maxSectionRatio = { id: 'nothing', ratio: 0}
+    for (let id in sectionRatios) {
+      if (sectionRatios[id] > maxSectionRatio.ratio) {
+        maxSectionRatio.ratio = sectionRatios[id]
+        maxSectionRatio.id = id
+      }
+    }
+    setCurrSection(maxSectionRatio.id)
+  }, [sectionRatios])
 
   return (
     <>
     {sectionList && sectionList.length > 0 &&
       <nav className={'team-page-side-bar'}>
         <ul>
-          {sectionList.map(sectionToLink)}
+          {sectionList.map(section => sectionToLink(section, currSection))}
         </ul>
       </nav>}
     </> 
   )
 }
 
-
-const sectionToLink = (section) => {
+const sectionToLink = (section, currentId) => {
   return (
     <li key={section.name} onClick={() => scrollToRef(section.ref)}>
-      {section.name}
+      {section.id === currentId ? <b>{section.name}</b> : section.name}
     </li>
   )
 }
