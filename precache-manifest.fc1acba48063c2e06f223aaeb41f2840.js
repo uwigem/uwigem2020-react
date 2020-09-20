736f50da11851d75969fc51e9912efe72a05820f
VictorShan <38676836+VictorShan@users.noreply.github.com>
2020-09-20 02:50:16 +0000

    Deploying to gh-pages from  @ a422d4c59deea53688f1d026488f30ecf75c556b 🚀

---
+++ precache-manifest.fc1acba48063c2e06f223aaeb41f2840.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866b53f60161d4c3f82f6ad0ce6b6d3a",
+    "url": "/uwigem2020-react/index.html"
+  },
+  {
+    "revision": "2bd35f68c1d507b1054a",
+    "url": "/uwigem2020-react/static/css/2.451a169c.chunk.css"
+  },
+  {
+    "revision": "b3dd06352d697e1ad50b",
+    "url": "/uwigem2020-react/static/css/main.02451c19.chunk.css"
+  },
+  {
+    "revision": "2bd35f68c1d507b1054a",
+    "url": "/uwigem2020-react/static/js/2.1757dc43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/uwigem2020-react/static/js/2.1757dc43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3dd06352d697e1ad50b",
+    "url": "/uwigem2020-react/static/js/main.706cdd04.chunk.js"
+  },
+  {
+    "revision": "a8d2af4669823f4f9962",
+    "url": "/uwigem2020-react/static/js/runtime-main.3b2725cc.js"
+  },
+  {
+    "revision": "282a92dce1de4afc251a3154784a07b1",
+    "url": "/uwigem2020-react/static/media/gmail.282a92dc.svg"
+  },
+  {
+    "revision": "1a91dffeb54d705fae5039ca8b6ed8a2",
+    "url": "/uwigem2020-react/static/media/gmailInactive.1a91dffe.svg"
+  },
+  {
+    "revision": "89306e4b80c1dcb692fafa258d090f7c",
+    "url": "/uwigem2020-react/static/media/pic2.89306e4b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Brepo-name%7D/precache-manifest.5af7a9b32be818bb4dd7fdf18a8c8fae.js"
+  "/uwigem2020-react/precache-manifest.fc1acba48063c2e06f223aaeb41f2840.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Brepo-name%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uwigem2020-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
