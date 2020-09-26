a9b987d39e9e87195b9bc55d70463a8df031d0ae
Jennifer Tao <1365673620@qq.com>
2020-09-25 19:00:52 -0700

    update synbio for everyone image on get imvolved page

--- src/components/involve/SynbioForEveryone/SynbioForEveryone.js
+++ src/components/involve/SynbioForEveryone/SynbioForEveryone.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './SynbioForEveryone.css';
-import Image from 'react-bootstrap/Image'
-import Figure from 'react-bootstrap/Figure'
 
 /**
  * @returns {React.Component}
@@ -14,32 +12,33 @@ const SynbioForEveryone = (props) => {
     const title = "Synbio for Everyone";
 
     return <>
-        <div className="m-5">
-            <div className="mb-4">
-                <div class="container">
-                    <div class="d-flex justify-content-between" style={{
-                        width: 'auto',
-                        height: '100%'
-                    }}>
-                        <img class="synbio-img" width="40%" height="auto" src="https://placekitten.com/430/553"/>
-                        <div class="ml-5">
-                            <img class="synbio-img" width="100%" height="80%" src="https://placekitten.com/417/439"/>
-                            <div className="synbio-view-more mt-5">
-                                <a href="http://students.washington.edu/uwigem/">-{'>'}View More</a>
-                            </div>
+        <div class="m-5">
+            <div class="container">
+                <div class="d-flex justify-content-between" style={{
+                    width: 'auto',
+                    height: '100%'
+                }}>
+                    <div class="synbio-img-left" 
+                        style={{backgroundImage: 'url(https://placekitten.com/500/600)'}}/>
+                    <div>
+                        <div class="synbio-img-right"
+                            style={{backgroundImage: 'url(https://placekitten.com/800/500)'}}/>
+                        <div className="synbio-view-more mt-5">
+                            <a href="http://students.washington.edu/uwigem/">-{'>'}View More</a>
                         </div>
                     </div>
-                    <div class="synbio-text-block-1">
-                        <h2>SynBio</h2>
-                    </div>
-                    <div class="synbio-text-block-2">
-                        <h2>for</h2>
-                    </div>
-                    <div class="synbio-text-block-3">
-                        <h2>Everyone</h2>
-                    </div>
+                </div>
+                <div class="synbio-text-block-1">
+                    <h2>SynBio</h2>
+                </div>
+                <div class="synbio-text-block-2">
+                    <h2>for</h2>
+                </div>
+                <div class="synbio-text-block-3">
+                    <h2>Everyone</h2>
                 </div>
             </div>
+            
             <div class="synbio-text-main">
                 {props.text}
             </div>
