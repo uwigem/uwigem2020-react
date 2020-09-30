aa98494ee420734ba1ac18e2216b53ad1d88b48f
Max Zhou <zhouyifan0904@gmail.com>
2020-09-30 15:47:26 -0700

    finished email component

--- src/components/email/Email.tsx
+++ src/components/email/Email.tsx
@@ -2,29 +2,49 @@ import React, { useState } from 'react';
 
 import firebase from '../../Firebase';
 
+import { Paper } from '@material-ui/core';
 import s from './Email.module.css';
 
+const receivingAddr = 'zhouyifan0904@gmail.com';
 
 const Email = () => {
   const [addr, setAddr] = useState<string>('');
   const [msg, setMsg] = useState<string>('');
   const [sbj, setSbj] = useState<string>('');
   return <div className={s.root}>
-    <form onSubmit={e => send(e, addr, sbj, msg)}>
+    <Paper className={s.sheet} elevation={5} >
+    <form onSubmit={e => {
+          send(e, addr, sbj, msg);
+          setAddr(''); setMsg(''); setSbj('');
+        }}>
+      <h2>Contact Us</h2>
       <div className={s.cell}>
-        Your Address: <input type='email' value={addr} onChange={e => setAddr(e.target.value)} />
+        <input
+          type='email'
+          placeholder='Your address (example@gmail.com)'
+          value={addr}
+          onChange={e => setAddr(e.target.value)} />
       </div>
 
       <div className={s.cell}>
-        Subject: <input type='text' value={sbj} onChange={e => setSbj(e.target.value)} />
+        <input
+          type='text'
+          placeholder='Subject (optional)'
+          value={sbj}
+          onChange={e => setSbj(e.target.value)} />
       </div>
 
       <div className={s.cell}>
-        Message: <input type='text' value={msg} onChange={e => setMsg(e.target.value)} />
+        <textarea
+          rows={6}
+          placeholder='Leave your message and we will get back to you ASAP :)'
+          value={msg}
+          onChange={e => setMsg(e.target.value)} />
       </div>
 
-      <button type='submit' value='Submit'>Send</button>
+      <div className={s.submitContainer}><button type='submit' value='Submit'>Submit</button></div>
     </form>
+    </Paper>
   </div>
 }
 
@@ -37,16 +57,16 @@ const send = (
   sbj: string,
   msg: string
 ): void => {
-  console.log('sending email');
   e.preventDefault();
   if (valid(addr, sbj, msg)) {
     mailRef.add({
-      to: [addr],
+      to: [receivingAddr],
       message: {
-        subject: sbj,
+        subject: `Message From <${addr}>: ${sbj? sbj : '<No Subject>'}`,
         text: msg
       }
     });
+    alert('Message Sent!');
   }
 }
 
@@ -60,11 +80,6 @@ const valid = (
   // test email addr format
   if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(addr)) {
     alert('Invalid email address');
-    return false;
-  }
-
-  if (sbj.length == 0) {
-    alert('Empty subject line');
     return false;
   }
 
