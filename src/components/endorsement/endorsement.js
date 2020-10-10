6c0d023d75d8e7f4182c2b9db1dd7ac0de227088
Diana Dai <dianadai.dy@gmail.com>
2020-10-10 16:39:58 -0700

    add business card to endorsement

---
+++ src/components/endorsement/endorsement.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BusinessCard from './BusinessCard/BusinessCard'
+
+/**
+ * @returns {React.Component}
+ */
+const Endorsement = (props) => {
+    return <>
+        <BusinessCard />
+    </>;
+}
+
+export default Endorsement;
