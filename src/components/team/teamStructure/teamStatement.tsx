da8b427fafb20ab70834c420789a7b2a78f51813
Victor Shan <victorshanvs@gmail.com>
2020-10-17 16:39:04 -0700

    team structure refactor

--- src/components/team/teamStructure/teamStatement.tsx
+++ src/components/team/teamStructure/teamStatement.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Fade from 'react-reveal/Fade';
 import './teamStatement.css';
 
+type propsType = {
+    teamName: string,
+    statement: string,
+    highlight: string,
+    statementMore: string,
+    example: string
+}
+
 /**
  * @param teamName the name of the team to be displayed
  * @param statement the team statment, what they says
@@ -10,23 +18,21 @@ import './teamStatement.css';
  * @param example the deliverable examples of the team
  * @returns {React.Component}
  */
-const TeamStatement = (props) => {
+export default function TeamStatement({ teamName, statement, highlight, statementMore, example}: propsType) {
 
     return <>
         <div className="team-statement-div">
             <Fade duration={1200}>
-            <div className="team-statement-title">{props.teamName} Team Says:</div>
+            <div className="team-statement-title">{teamName} Team Says:</div>
             <div className="team-statement-main">
-                {props.statement}
-                <b><i>{props.highlight}</i></b>
-                {props.statementMore}</div>
+                {statement}
+                <b><i>{highlight}</i></b>
+                {statementMore}</div>
             <div className="team-statement-example">
                 <b>Our Deliverable Examples: </b>
-                {props.example}</div>
+                {example}</div>
             <div className="team-statement-link">View Our Team Members</div>
             </Fade>
 		</div>
     </>;
 }
-
-export default TeamStatement;
