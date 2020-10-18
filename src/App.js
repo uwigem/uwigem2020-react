3e8601ee74641857f38885502f120cfdddd38733
Jennifer Tao <1365673620@qq.com>
2020-10-18 16:41:08 -0700

    fix style

--- src/App.js
+++ src/App.js
@@ -13,11 +13,8 @@ import NavBar from './components/navbar/navbar';
 import HomePage from './components/home/home';
 import TeamPage from './components/team/teamPage';
 import Footer from './components/footer/footer';
-import GetInvolve from './components/involve/involve';
 import ArchivePage from './components/archive/archive';
-import OurTeamPage from './components/team/OurTeamPage';
 import EndorsementPage from './components/endorsement/endorsement';
-import ProjectPage from './components/construction/UnderConstruction';
 import UnderConstruction from './components/construction/UnderConstruction';
 
 function App() {
