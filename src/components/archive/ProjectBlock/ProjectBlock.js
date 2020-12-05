41740daf3e441db9e8510d3ba35ad4b3f18aa9f2
Diana Dai <dianadai.dy@gmail.com>
2020-12-05 11:29:31 -0800

    edit archive image and text to udpate

--- src/components/archive/ProjectBlock/ProjectBlock.js
+++ src/components/archive/ProjectBlock/ProjectBlock.js
@@ -48,6 +48,7 @@ const ProjectBlock = (props) => {
                         <h1>{props.project.name}</h1>
                         <h2>{props.project.year}</h2>
                     </div>
+                    <h2 className="project-block-professor">{props.project.professor}</h2>
                     <h2 className="project-block-professor"><a href={props.project.link}>Wiki Page</a></h2>
                     <p className="project-block-description">{props.project.description}</p>
                 </div>
