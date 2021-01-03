8cfbc4fb1c00f981fedaa52540465b03fe4d2db8
Diana Dai <dianadai.dy@gmail.com>
2021-01-02 16:52:11 -0800

    adding text and modify recruitment descriptions

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,6 +10,11 @@ const HomePage = props => {
   return (
     <>
       <HomeCarousel />
+      <div className="recruit-menu-description">
+        We are holding a <b>virtual information session on Saturday, January 9th, 11:00am - 12:30pm</b>. 
+        This will be held on zoom, <a href="https://igem-org.zoom.us/j/98707180183?pwd=RUdDMlhYQXJHSmZSTGJLd3dLS2w3QT09">click here for link to join</a>. 
+        A recording will be posted here shortly afterwards if you are unable to attend.
+      </div>
       <Hightlights />
       <Competition />
       <JoinUs />
--- src/components/involve/involve.js
+++ src/components/involve/involve.js
@@ -10,13 +10,12 @@ import './involve.css';
  */
 const GetInvolve = (props) => {
     const synbio = "Synbio for Everyone (SFE) is a nonprofit organization located in Washington State that was founded by members of the 2019 WA iGEM Outreach Team after receiving the Best Education and Public Engagement Award. SFE  strives to improve synthetic biology education accessibility and reduce educational equity gaps in STEM by writing curriculum and conducting educational outreach events. In the past, SFE’s curriculum has been translated into 16 different languages and implemented in a variety of settings with collaborators globally. SFE also promotes awareness about synthetic biology research and ethical issues by hosting a panel event featuring experts every year.  Overall, Synbio for Everyone strives for growth and collaboration across disciplines and across economic barriers, forging new paths in the future of synthetic biology education and its implications for the world."
-    // const text2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid enim dolore ipsa nihil blanditiis consectetur dolorem atque sint molestiae, ut dolores soluta perspiciatis temporibus quibusdam accusantium? Quo est dicta consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laboriosam voluptas nam vel corporis?";
-
+    
     return <>
         <h1 className="get-involve-title">Get Involved</h1>
         <hr className="get-involve-linebar"></hr>
         <RecruitmentMenu />
-        <Synbio text={synbio} />
+        {/* <Synbio text={synbio} /> */}
         {/* <Fundraising text1={text2} text2={text2}/> */}
     </>;
 }
