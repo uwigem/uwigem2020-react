e52e66990f704fe745a1a3c256e13de50c681307
Diana Dai <dianadai.dy@gmail.com>
2021-01-03 19:42:10 -0800

    add facebook event link

--- src/components/involve/Recruitment/RecruitmentMenu.tsx
+++ src/components/involve/Recruitment/RecruitmentMenu.tsx
@@ -34,7 +34,7 @@ export default function RecruitmentMenu() {
             <h2 className={styles.title}>Recruitment</h2>
             <div className={styles.infoSession}>
                     We are holding a <b>virtual information session on Saturday, January 9th, 11:00am - 12:30pm</b>. 
-                This will be held on zoom, go to the <a href="https://www.facebook.com/WashingtoniGEM">Facebook event</a> and join with link in the description. 
+                This will be held on zoom, go to the <a href="https://www.facebook.com/events/851619972292709">Facebook event</a> and join with link in the description. 
                 A recording will be posted here shortly afterwards if you are unable to attend.
             </div>
             <div className={styles.description}>
