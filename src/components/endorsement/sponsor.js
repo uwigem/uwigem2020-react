febbf34537ae3af3e1bd8cb26a29b81f86b3edff
Victor Shan <victorshanvs@gmail.com>
2021-02-13 17:21:34 -0800

    simplify endorsement page

--- src/components/endorsement/sponsor.js
+++ src/components/endorsement/sponsor.js
@@ -1,27 +1,16 @@
 // import './sponsor.css';
 import styles from './sponsor.module.css'
-import React, {useState} from 'react';
+import React from 'react';
 
 /**
  * @param sponsor sponsor information with {logo, name, link}
- * @param width sponsor card default width
- * @param height sponsor card default height
- * @param ratio percentage the sponsor card takes on the website width
  * @returns {React.Component} A component that displays a sponsor's information
  */
 const Sponsor = props => {
-	const [hover, toggleHover] = useState(false);
-	const [loaded, toggleLoaded] = useState(false);
-
 	return <>
 		<a className={styles.sponsorWrapper}
 			href={props.sponsor.link}>
 			<div className={styles.sponsor}
-				onMouseEnter={() => {
-					toggleHover(true);
-					toggleLoaded(true);
-				}}
-				onMouseLeave={() => toggleHover(false)}
 				>
 				<section className={styles.sponsorImg}
 					style={{
