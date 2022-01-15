3824bc7077a308f0137adf83eee7eb0571eff797
kuob <69408022+kuob@users.noreply.github.com>
2022-01-15 08:29:45 -0800

    Updated recording link

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,7 +13,7 @@ const HomePage = props => {
       <HomeCarousel />
       <div className={styles.infoSession}>
         Our next recruitment session is on 1/14/22 at 6pm (Zoom ID: 673 998 2327). Check out our previous recruitment session!
-        Click the links to see the <a href="https://www.youtube.com/watch?v=aa1j4rEWhDM&feature=youtu.be">recording</a> and the <a href="https://docs.google.com/presentation/d/1bCkQujJGBAyKfE3P7g6NP8DPPTTflzFzg3rek41Q3vE/edit?usp=sharing">slides</a>!
+        Click the links to see the <a href="https://www.youtube.com/watch?v=WS-_LD2kbUM">recording</a> and the <a href="https://docs.google.com/presentation/d/1bCkQujJGBAyKfE3P7g6NP8DPPTTflzFzg3rek41Q3vE/edit?usp=sharing">slides</a>!
       </div>
       
       <Competition />
