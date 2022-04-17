b3e1091d1dfefd6b930471bb1d224f60eb66d8ab
MilkyTea123 <63531427+MilkyTea123@users.noreply.github.com>
2022-04-16 23:29:05 -0700

    cleaned code
    deleted unnecessary parts that I had commented out

--- src/components/join-us/SocialMediaLink.tsx
+++ src/components/join-us/SocialMediaLink.tsx
@@ -12,7 +12,6 @@ export default function SocialMediaLink(props:data) {
   return (
     <a href={props.url} target={"_blank"} rel={"noopener noreferrer"}>
       <div className={styles.link}>
-        {/* <img src={props.iconSrc} alt={props.label} className={styles.img}/> */}
         <Icon icon={props.iconSrc}/>
         <p className={styles.p}>{props.label}</p>
       </div>
